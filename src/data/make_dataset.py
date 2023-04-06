3a7fba0267be202b47f29bd45afda3a02c4a3fa8
Vigrel <eller.vini@gmail.com>
2023-04-06 11:53:32 -0300

    fix: resolve ip block for multiple requests problem with time.sleep

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 import os
+import time
 import zipfile
 from datetime import datetime, timedelta
 
@@ -13,10 +14,9 @@ class Data:
         folder = f"data/raw/{coin}/{interval}/"
         os.makedirs(folder, exist_ok=True)
         base_url = "https://data.binance.vision/data/spot/daily/klines"
-        downloaded_files = []
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [
+            [
                 executor.submit(
                     Data.download_file,
                     f"{base_url}/{coin}/{interval}/{coin}-{interval}-{(datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d')}.zip",
@@ -25,27 +25,29 @@ class Data:
                 for i in range(1, days + 1)
             ]
 
-            for future in concurrent.futures.as_completed(futures):
-                downloaded_files.append(future.result())
-
-        with open(f"{folder}/downloaded_files.txt", "w") as output:
-            output.write("\n".join(downloaded_files))
+        Data.zip2csv(folder)
 
     @staticmethod
-    def download_file(url: str, folder: str) -> str:
+    def download_file(url: str, folder: str):
         file_name = url.split("/")[-1]
+        time.sleep(2)
+
         with requests.get(url, stream=True) as r, open(
             f"{folder}/{file_name}", "wb"
         ) as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
-        with zipfile.ZipFile(f"{folder}/{file_name}", "r") as zip_ref:
-            zip_ref.extract(f"{file_name[:-4]}.csv", folder)
-        os.remove(f"{folder}/{file_name}")
-        return file_name
 
     @staticmethod
-    def raw2csv(folder: str):
+    def zip2csv(folder: str):
+        files = os.listdir(folder)
+        for file_name in files:
+            with zipfile.ZipFile(f"{folder}/{file_name}", "r") as zip_ref:
+                zip_ref.extract(f"{file_name[:-4]}.csv", folder)
+            os.remove(f"{folder}/{file_name}")
+
+    @staticmethod
+    def concat_csv(folder: str):
         files = os.listdir(folder)
         columns = [
             "Id",
@@ -69,6 +71,3 @@ class Data:
         new_folder = folder.replace("raw", "processed")
         os.makedirs(new_folder, exist_ok=True)
         main_df.to_csv(f"{new_folder}/data.csv")
-
-
-Data.raw2csv("data/raw/BTCUSDC/5m")
