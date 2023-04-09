0f3c0ce3f5eeed8f3d9bb479e7a1c7ea8fedfde0
Vigrel <eller.vini@gmail.com>
2023-04-09 15:21:04 -0300

    feat: add PyEMD model usage. Generate IMFs for data close prices

--- src/model/EEMD.py
+++ src/model/EEMD.py
@@ -1,34 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 from PyEMD import EEMD
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Define signal
-t = np.linspace(0, 1, 200)
+data = pd.read_csv("data/processed/BTCUSDC/5m/data_processed.csv")
+signal = data["Close"].to_numpy()
 
-sin = lambda x, p: np.sin(2 * np.pi * x * t + p)
-S = sin(1, 0)
-
-# Assign EEMD to `eemd` variable
 eemd = EEMD()
+IMFs = eemd(signal)
+np.save("data/processed/IMFs.npy", IMFs)
 
-emd = eemd.EMD
-emd.extrema_detection = "parabol"
+plt.figure(figsize=(25, 10))
 
-# Execute EEMD on S
-eIMFs = eemd.eemd(S, t)
-nIMFs = eIMFs.shape[0]
-
-# Plot results
-plt.figure(figsize=(12, 9))
-plt.subplot(nIMFs + 1, 1, 1)
-plt.plot(t, S, "r")
-
-for n in range(nIMFs):
-    plt.subplot(nIMFs + 1, 1, n + 2)
-    plt.plot(t, eIMFs[n], "g")
-    plt.ylabel("eIMF %i" % (n + 1))
-    plt.locator_params(axis="y", nbins=5)
-
-plt.xlabel("Time [s]")
-plt.tight_layout()
-plt.show()
+for i, IMF in enumerate(IMFs):
+    plt.subplot(len(IMFs), 1, i + 1)
+    plt.plot(IMF, label="IMF {}".format(i + 1))
+    plt.legend()
+plt.savefig("reports/IMFs.png")
